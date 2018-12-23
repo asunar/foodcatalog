01bf039faf42204385b3f0b0b928ab6b9ab4d32b
Alper Sunar <ASunar@Paylocity.com>
2018-12-23 08:10:12 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class FoodCatalog extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <FoodCardRow foods={this.props.foods} />
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class FoodCardRow extends React.Component {
+  render() {
+    const foods = this.props.foods;
+    return (
+      <div className="row">
+        {foods.map(foodItem => (
+          <FoodCard key="1" food={foodItem} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class FoodCard extends React.Component {
+  render() {
+    const { category, name, quantity, weight, description } = this.props.food;
+    return (
+      <div className="col" style={{ backgroundColor: "#bfe0bf" }}>
+        <img src="/images/tomato.png" alt="tomato" />
+        <div className="text" />
+        <span className="badge badge-pill badge-primary">{category}</span>
+        <h3>{name}</h3>
+        <div>
+          {quantity} - {weight}
+        </div>
+        <div>{description}</div>
+      </div>
+    );
+  }
+}
+const FOODS = [
+  {
+    key: 1,
+    category: "Vegetables",
+    name: "Tomato",
+    quantity: 2,
+    weight: "3g",
+    description: "I am a tomato"
+  }
+];
+
+ReactDOM.render(<FoodCatalog foods={FOODS} />, document.getElementById("root"));
