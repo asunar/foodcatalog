e1268756388535788f38d5e15d6b0a6329cb136e
Alper Sunar <ASunar@Paylocity.com>
2019-01-03 18:03:15 -0600

    Updates

---
+++ precache-manifest.3991cf4be51aa6c3a90f9fa488ddcfb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc63f0652f70cf7434d",
+    "url": "/foodcatalog/static/js/runtime~main.1fc63f06.js"
+  },
+  {
+    "revision": "b164f98cb91775798572",
+    "url": "/foodcatalog/static/js/main.b164f98c.chunk.js"
+  },
+  {
+    "revision": "7a60306f0c814e8ba225",
+    "url": "/foodcatalog/static/js/1.7a60306f.chunk.js"
+  },
+  {
+    "revision": "b164f98cb91775798572",
+    "url": "/foodcatalog/static/css/main.70e4de0a.chunk.css"
+  },
+  {
+    "revision": "7a60306f0c814e8ba225",
+    "url": "/foodcatalog/static/css/1.b3e6e6ff.chunk.css"
+  },
+  {
+    "revision": "42734c1f81fa7ba0ec7924b363d6288f",
+    "url": "/foodcatalog/index.html"
+  }
+];
