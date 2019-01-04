f6cd4bee7ea14f10f71824272b6fabc9d220e57b
Alper Sunar <ASunar@Paylocity.com>
2019-01-03 18:17:41 -0600

    Updates

---
+++ precache-manifest.0a3266007a538bb8f07700b56449a0ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc63f0652f70cf7434d",
+    "url": "/foodcatalog/static/js/runtime~main.1fc63f06.js"
+  },
+  {
+    "revision": "668c65d4b95b0c7b47bc",
+    "url": "/foodcatalog/static/js/main.668c65d4.chunk.js"
+  },
+  {
+    "revision": "572947d9a0765bcd37c9",
+    "url": "/foodcatalog/static/js/1.572947d9.chunk.js"
+  },
+  {
+    "revision": "668c65d4b95b0c7b47bc",
+    "url": "/foodcatalog/static/css/main.fdf72439.chunk.css"
+  },
+  {
+    "revision": "572947d9a0765bcd37c9",
+    "url": "/foodcatalog/static/css/1.ff00d2ce.chunk.css"
+  },
+  {
+    "revision": "7d048d25632136b3905d854a0c0f269e",
+    "url": "/foodcatalog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fc77b2f4936fb27f922b7ff161a93cb.js"
+  "/foodcatalog/precache-manifest.0a3266007a538bb8f07700b56449a0ad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/foodcatalog/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
